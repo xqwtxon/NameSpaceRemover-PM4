b2e01f8cdd1ee1dcc046b4dd20a287dbf9ef0cae
VisKa_RaZe <104200671+Zelurium12121@users.noreply.github.com>
2022-05-25 21:30:08 +0200

    Update Main.php

--- src/JackMD/NameSpaceRemover/Main.php
+++ src/JackMD/NameSpaceRemover/Main.php
@@ -41,7 +41,7 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener{
 
-	public function onEnable(): void{
+	public function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info("Plugin Enabled.");
 	}
